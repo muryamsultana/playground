aa8ad28d7a95ccff6a1131be39d942fd4e3033d4
Muryam <muryam.sultana@gmail.com>
2025-10-25 19:07:55 +0500

    Update weather.php

--- weather.php
+++ weather.php
@@ -12,19 +12,19 @@ if (!defined('ABSPATH')) {
 }
 
 class OpenWeather_Shortcode {
-    public $api_key_option = 'openweather_api_key';
-    public $key =  'bdabe763ab4c3757cd2754c5af5148ec';
+    private $api_key_option = 'openweather_api_key';
+    private $key =  'bdabe763ab4c3757cd2754c5af5148ec';
 
     public function __construct() {
         add_action('admin_menu', [$this, 'add_settings_page']);
         add_shortcode('weather', [$this, 'shortcode_handler']);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_styles']);
-        update_option($this->api_key_option, $this->key);
 
     }
 
     // Add settings page
     public function add_settings_page() {
+        update_option($this->api_key_option, $this->key);
         add_options_page(
             'OpenWeatherAPI Settings',
             'OpenWeatherAPI',
